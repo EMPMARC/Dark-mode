5e3ba563e905d06e526beadfb222acbee9f59722
EMPMARC <161944929+EMPMARC@users.noreply.github.com>
2024-03-05 02:14:36 +0200

    Add files via upload

---
+++ DarkMode/DarkMode/DarkMode/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace DarkMode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.lblmsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.AutoSize = true;
+            this.btnProcess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProcess.Location = new System.Drawing.Point(118, 41);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 25);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // lblmsg
+            // 
+            this.lblmsg.AutoSize = true;
+            this.lblmsg.Location = new System.Drawing.Point(413, 162);
+            this.lblmsg.Name = "lblmsg";
+            this.lblmsg.Size = new System.Drawing.Size(111, 13);
+            this.lblmsg.TabIndex = 1;
+            this.lblmsg.Text = "This is a test message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblmsg);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label lblmsg;
+    }
+}
+
---
+++ DarkMode/DarkMode/DarkMode/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DarkMode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeUI("UIMode");
+        }
+
+        private void InitializeUI(string key)
+        {
+            try
+            {
+                var uiMode = ConfigurationManager.AppSettings[key];
+                if(uiMode == "light")
+                {
+                    btnProcess.Text = "Enable Dark Mode";
+                    lblmsg.Text = "Light mode is enabled now";
+                    this.ForeColor= Color.FromArgb(47, 54, 64);
+                    this.BackColor = Color.FromArgb(245,246,250);
+                    ConfigurationManager.AppSettings[key] = "dark";
+                }
+                else
+                {
+                    btnProcess.Text = "Enable Light Mode";
+                    lblmsg.Text = "Dark mode is enabled now";
+                    this.ForeColor = Color.FromArgb(245, 246, 250);
+                    this.BackColor = Color.FromArgb(47,54,64);
+                    ConfigurationManager.AppSettings[key] = "light";
+                }
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            InitializeUI("UIMode");
+            //https://www.youtube.com/watch?v=X0wJusugq-E
+        }
+    }
+}
